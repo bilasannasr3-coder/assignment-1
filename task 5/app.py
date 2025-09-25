b9f63d4b93474b02d2e6199857f38d687b8fc237
bilasannasr3-coder <bilasannasr3@gmail.com>
2025-09-25 03:25:58 +0300

    add task 5

---
+++ task 5/app.py
@@ -0,0 +1,80 @@
+from flask import Flask ,render_template , request , redirect
+import webbrowser
+from threading import Timer
+import os
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+app = Flask(__name__,template_folder='template')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(300),nullable=False)
+    date_created = db.Column(db.DateTime, default= datetime.utcnow)
+
+    def __repr__(self):
+       return '<Task %r>' % self.id
+    
+with app.app_context():
+    db.create_all()
+          
+    
+@app.route('/', methods=['POST','GET'])
+
+def index():
+    if request.method == 'POST':
+       task_content = request.form['content']
+       new_task = Todo(content=task_content)
+
+       try:
+           db.session.add(new_task)
+           db.session.commit()
+           return redirect('/')
+       except:
+           return "There was an issue adding your task"
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task '
+    
+@app.route('/update/<int:id>', methods=['GET','POST'])   
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+       task.content =request.form['content']
+
+       try: 
+           db.session.commit()
+           return redirect('/')
+       
+       except:   
+        return 'There was a problem updating that task'
+       
+    else:
+        return render_template('update.html', task=task )
+    
+
+def open_browser():
+    if not os.environ.get("WERKZEUG_RUN_MAIN"):
+       webbrowser.open("http://127.0.0.1:5000/")
+
+if __name__ == "__main__":
+    Timer(1, open_browser).start()
+    app.run(debug=True)
+
+
